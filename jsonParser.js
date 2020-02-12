2e85e68baa8bd3308330208c7334efb534606edb
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-02-12 11:04:57 +0530

    Implemented null parser

---
+++ jsonParser.js
@@ -0,0 +1,11 @@
+function nullParser(input) {
+  if (input.startsWith('{"null')) {
+    return [null, input.slice(4)]
+  }
+
+  return null
+}
+
+console.log(nullParser('nullabcd'))
+console.log(nullParser('abcdnull'))
+console.log(nullParser('{"null":"John", "age":31, "city":"New York"}'))
