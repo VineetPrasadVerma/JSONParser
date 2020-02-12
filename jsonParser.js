bd16e62e96ba992569d4cb97046d7a6abc894fa4
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-02-12 11:21:26 +0530

    Added boolean Parser

--- jsonParser.js
+++ jsonParser.js
@@ -8,3 +8,19 @@ function nullParser(input) {
 console.log(nullParser('nullabcd'))
 console.log(nullParser('abcdnull'))
 // console.log(nullParser('{"null":"John", "age":31, "city":"New York"}'))
+
+function booleanParser(input) {
+  if (input.startsWith('true')) {
+    return [true, input.slice(4)]
+  }
+
+  if (input.startsWith('false')) {
+    return [false, input.slice(5)]
+  }
+
+  return null
+}
+
+console.log(booleanParser('trueCorrect'))
+console.log(booleanParser('falseWrong'))
+console.log(booleanParser('CorrectTrue'))
