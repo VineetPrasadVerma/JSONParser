025eecb3c8c65abe90e2989befe5c608478950d1
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-02-15 20:00:27 +0530

    Partially implemented String Parser

---
+++ stringParser.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+const data = fs.readFileSync(
+  'D:\\Geekskool\\JSONParser\\inputString.txt',
+  'utf8'
+)
+
+function stringParser (data) {
+  const ans = []
+  const specialCharacter = {
+    '"': '"',
+    '\\': '\\',
+    '/': '/',
+    b: '\b',
+    f: '\f',
+    n: '\n',
+    r: '\r',
+    t: '\t'
+  }
+
+  // console.log(specialCharacter)
+
+  // console.log(data)
+  if (!data.startsWith('"')) return null
+  data = data.slice(1)
+  while (data.length !== 0 && data[0] !== '"') {
+    if (data[0] === '\\') {
+      if (specialCharacter[data[1]]) {
+        ans.push(specialCharacter[data[1]])
+        data = data.slice(2)
+        continue
+      }
+      if (data[1] === 'u') {
+        const hexDigits = data.slice(2, 6)
+        if (hexDigits.match(/[a-f|A-F|0-9]{4}/)) {
+          const actualCharacter = String.fromCodePoint(parseInt(hexDigits, 16))
+          if (actualCharacter) {
+            data = data.slice(6)
+            ans.push(actualCharacter)
+          } else {
+            return null
+          }
+        } else {
+          return null
+        }
+      } else {
+        return null
+      }
+    }
+
+    ans.push(data[0])
+    data = data.slice(1)
+
+    // return [ans.join(''), data]
+    // for (let i = 0; i < data.length; i++) {
+    //   if (data[i] === '\\') {
+    //     // console.log(data[i + 1])
+    //     const temp = specialCharacter[data[i + 1]]
+    //     if (temp) {
+    //       ans.push(temp)
+    //       i += 1
+    //     } else {
+    //       return null
+    //     }
+    //   } else {
+    //     ans.push(data[i])
+    //   }
+    // }
+  }
+  return [ans, data]
+}
+console.log(stringParser(data))
+const t = stringParser(data)[0].join('')
+console.log(t)
+// console.log([t, 0])
