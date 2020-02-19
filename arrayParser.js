115abdccf87ab2a0d94614901b372b9e5e2965ff
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-02-19 16:19:18 +0530

    Updated logic of array parser

--- arrayParser.js
+++ arrayParser.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const valueParser = require('./valueParser')
+const objectParser = require('./objectParser')
 
 const data = fs.readFileSync(
   'D:\\Geekskool\\JSONParser\\inputString.txt',
@@ -9,27 +10,19 @@ const data = fs.readFileSync(
 function arrayParser (data) {
   const ans = []
   if (!data.startsWith('[')) return null
-  data = data.slice(1)
-  data = data.trim()
+  data = data.slice(1).trim()
   while (data.length !== 0 && data[0] !== ']') {
-    const valueParserValue = valueParser(data) || arrayParser(data)
+    data = data.trim()
+    const valueParserValue =
+      valueParser(data) || arrayParser(data) || objectParser(data)
     if (!valueParserValue) return null
-    else {
-      ans.push(valueParserValue[0])
-      data = valueParserValue[1]
-      data = data.trim()
-      if (data[0] === ',') {
-        data = data.slice(1)
-        data = data.trim()
-        if (data[0] === ']') return null
-      } else {
-        if (data[0] !== ']') return null
-      }
-    }
+    ans.push(valueParserValue[0])
+    data = valueParserValue[1].trim()
+    if (data[0] !== ',') break
+    data = data.slice(1).trim()
+    if (data[0] === ']') return null
   }
-
-  if (data[0] === ']') return [ans, data.slice(1)]
-  return null
+  return data[0] === ']' ? [ans, data.slice(1)] : null
 }
 
 console.log(arrayParser(data))
